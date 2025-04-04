c23dbafbe71551401b35e7573ce22c8ef61576a2
Andre Abdala Noel <andre.noel@docente.pr.senac.br>
2025-04-03 21:30:59 -0300

    Removendo o amarelo

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-document.querySelector('header').style.background = 'yellow';
-document.querySelector('footer').style.backgroundColor = 'yellow';
+// document.querySelector('header').style.background = 'yellow';
+// document.querySelector('footer').style.backgroundColor = 'yellow';
